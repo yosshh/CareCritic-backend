3282a77aa484ba1180bba2cc243851ca07daf0b4
Yash Shukla <yshukla2775@gmail.com>
2024-09-12 13:59:16 +0530

    all setup configured

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,4 +15,12 @@ app.use(express.urlencoded({extended: true, limit: JSON_LIMIT}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+//route import 
+import userRouter from './routes/user.routes.js'
+
+
+//route declaration
+app.use("/api/v1/users", userRouter)
+
 export { app }
