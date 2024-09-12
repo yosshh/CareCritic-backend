3dd5a26c2585071fd9e2cc099eecd2e255209575
Yash Shukla <yshukla2775@gmail.com>
2024-09-12 13:22:03 +0530

    add user and doctor models

--- backend/Readme.md
+++ backend/Readme.md
@@ -1,3 +1,27 @@
 # CARECRITIC - BACKEND
 
-This is a backend folder for server side of the application CARECRITIC
+
+## DESCRIPTION
+This is a backend folder for the application CARECRITIC, built with Expres, Mongodb and Node.js.
+
+## Installation
+1. Clone the repo: https://github.com/yosshh/CareCritic-backend/tree/main/backend
+2. Navigate to the project directory: cd backend
+3. Install dependencies: 
+- npm nodemon
+- npm prettier
+- npm bcrypt
+- npm express
+- npm jsonwebtoken
+- npm cors
+- npm cookie-parser
+- npm dotenv
+- npm npm mongoose
+
+
+## Technologies
+- Node.js
+- Express
+- MongoDB
+- Cloudinary
+                        
---
+++ backend/src/models/user.models.js
@@ -0,0 +1,78 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "UserName is required"],
+      unique: [true, "This username is already taken"],
+      lowercase: [true, "username should be in Lower Case"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "This email is already registered with another username"],
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: [true, "Full Name is required"],
+      trim: true,
+    },
+    profileImage: {
+      type: String, // cloudinary url
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+export const User = mongoose.model("User", userSchema);
