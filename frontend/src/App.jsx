2e81cbc6c41926488c9a9a650c618e1f345347f3
Yash Shukla <yshukla2775@gmail.com>
2024-09-23 05:51:38 +0530

    added navbarSection, feautreSection, testimonialSection and fix some bugs in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
+import FeatureSection from "./components/FeatureSection"
+import HeroSection from "./components/HeroSection"
+import Navbar from "./components/Navbar"
+import Testimonials from "./components/Testimonials"
 
-import './App.css'
 
 function App() {
   
   return (
     <>
-      
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <FeatureSection />
+      </div>
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Testimonials />
+      </div>
     </>
   )
 }
